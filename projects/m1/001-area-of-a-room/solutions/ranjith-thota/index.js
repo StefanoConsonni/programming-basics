00fa58561a07b688b937ace3457054ff371553d4
Ranjith Thota <amailtoranjith@gmail.com>
2023-01-28 20:09:28 +0100

    Solution to find the area of a room

---
+++ projects/m1/001-area-of-a-room/solutions/ranjith-thota/index.js
@@ -0,0 +1,17 @@
+let length = parseInt(prompt("Enter Length of the room in meters"));
+let width = parseInt(prompt(" Super, Now Enter Width of the room in meters"));
+
+let area = length*width;
+
+
+
+if(isNaN(area))
+{
+  alert(`make sure you enter right values`);
+    
+}
+else {
+    alert(`The area of the room is ${area}`);
+}
+
+
