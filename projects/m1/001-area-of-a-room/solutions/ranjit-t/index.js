115038fe9f18b127dda368321ec9744c93cfafe5
Ranjit T <121737263+ranjit-t@users.noreply.github.com>
2023-01-29 14:22:20 +0100

    Update index.js

--- projects/m1/001-area-of-a-room/solutions/ranjit-t/index.js
+++ projects/m1/001-area-of-a-room/solutions/ranjit-t/index.js
@@ -1,17 +1,15 @@
-let length = parseInt(prompt("Enter Length of the room in meters"));
-let width = parseInt(prompt(" Super, Now Enter Width of the room in meters"));
+const length = parseInt(prompt("Enter Length of the room in meters"));
+const width = parseInt(prompt(" Super, Now Enter Width of the room in meters"));
 
-let area = length*width;
+const area = length*width;
 
 
 
 if(isNaN(area))
 {
-  alert(`make sure you enter right values`);
+  alert("make sure you enter right values");
     
 }
 else {
     alert(`The area of the room is ${area}`);
 }
-
-
